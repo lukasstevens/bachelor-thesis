81c8a2b269c61e1bb005124d472d47f99bb468a2
Lukas Stevens <mail@lukas-stevens.de>
2017-02-19 18:01:10 +0100

    Use shared pointer instead of raw pointer
    -Avoids manual memory management
    -Makes copying of nodes without creating deep copies possible

--- code/cpp/src/Partition.cpp
+++ code/cpp/src/Partition.cpp
@@ -5,51 +5,42 @@
 #include<unordered_set>
 #include<string>
 #include<sstream>
+#include<memory>
 #include<list>
 
 typedef uint32_t u32;
 typedef int32_t i32;
-
 
 struct Node {
     public:
         u32 id;
         Node* parent;
         i32 parent_edge_weight;
-        Node* children;
-        u32 children_cnt;
+        std::shared_ptr<std::vector<Node>> children;
 
-        Node() : id(0), parent(nullptr), children(nullptr), children_cnt(0) {}
-        Node(u32 i, Node* p, i32 w): id(i), parent(p), parent_edge_weight(w), children(nullptr), children_cnt(0) {}
+        Node(u32 i, Node* p, i32 w): id(i), parent(p), parent_edge_weight(w), 
+            children(std::shared_ptr<std::vector<Node>>(new std::vector<Node>)) {}
 
-        ~Node() {
-            delete[] children;
-        }
+        static Node build_tree(std::unordered_map<u32, std::unordered_map<u32, i32>>& graph) {
+            Node root = Node(graph.begin()->first, nullptr, 0);
 
-        static Node* build_tree(std::unordered_map<u32, std::unordered_map<u32, i32>>& graph) {
-            Node* root = new Node(graph.begin()->first, nullptr, 0);
-
-            std::list<std::pair<u32, Node*>> queue;
-            queue.push_back(std::make_pair(0, root));
+            std::list<std::pair<u32, Node>> queue;
+            Node fake_root = Node(root.id, nullptr, 0);
+            fake_root.children->push_back(root);
+            queue.push_back(std::make_pair(0, fake_root));
             std::unordered_set<u32> visited;
 
             while (!queue.empty()) {
                 auto node_pair = queue.front();
                 queue.pop_front();
-                Node* curr_node = &node_pair.second[node_pair.first];
-                visited.insert(curr_node->id);
-
-                curr_node->children_cnt = static_cast<u32>(graph[curr_node->id].size());
-                if (curr_node->parent != nullptr) {
-                    --curr_node->children_cnt;
-                }
-                curr_node->children = new Node[curr_node->children_cnt]; 
+                Node curr_node = node_pair.second.children->at(node_pair.first);
+                visited.insert(curr_node.id);
 
                 u32 idx = 0;
-                for (auto neighbor : graph[curr_node->id]) {
+                for (auto neighbor : graph[curr_node.id]) {
                     if (visited.find(neighbor.first) == visited.end()) {
-                        curr_node->children[idx] = Node(neighbor.first, curr_node, neighbor.second);
-                        queue.push_back(std::make_pair(idx, curr_node->children));
+                        curr_node.children->push_back(Node(neighbor.first, &curr_node, neighbor.second));
+                        queue.push_back(std::make_pair(idx, curr_node));
                         idx += 1;
                     }
                 }
@@ -59,20 +50,22 @@ struct Node {
 
         std::string to_string() {
             std::stringstream stream;
-            std::list<Node*> queue;
-            queue.push_back(this);
+            std::list<Node> queue;
+            queue.push_back(*this);
 
             while (!queue.empty()) {
-                Node* curr_node = queue.front();
+                Node curr_node = queue.front();
                 queue.pop_front();
 
-                for (u32 child_idx = 0; child_idx < curr_node->children_cnt; ++child_idx) {
-                    stream << curr_node->id << " -> " << curr_node->children[child_idx].id << "[label=\"" << curr_node->children[child_idx].parent_edge_weight << "\"]\n";
-                    queue.push_back(&curr_node->children[child_idx]);
+                for (u32 child_idx = 0; child_idx < curr_node.children->size(); ++child_idx) {
+                    stream << curr_node.id << " -> " << (*curr_node.children)[child_idx].id;
+                    stream << "[label=\"" << (*curr_node.children)[child_idx].parent_edge_weight << "\"]\n";
+                    queue.push_back((*curr_node.children)[child_idx]);
                 }
             }
             return stream.str();
         }
+
 };
 
 int main() {
@@ -90,7 +83,7 @@ int main() {
         graph[to][from] = weight;
     }
 
-    Node* root = Node::build_tree(graph);
-    std::cerr << root->to_string() << std::endl;
-    delete(root);
+
+    Node root = Node::build_tree(graph);
+    std::cerr << root.to_string() << std::endl;
 }
