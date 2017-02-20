e757188d52a818680429f9c33e4833a7e6ad040f
Lukas Stevens <mail@lukas-stevens.de>
2017-02-20 17:32:58 +0100

    Fix indexing

--- code/cpp/src/main/Node.cpp
+++ code/cpp/src/main/Node.cpp
@@ -13,22 +13,33 @@ Node::Node(IdType i, Node* p, EdgeWeightType w) : id(i), parent(p), parent_edge_
 
 Node Node::build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree) {
     Node root(tree.begin()->first, nullptr, 0);
-
-    std::list<Node*> queue;
-    queue.push_back(&root);
+    std::list<std::pair<size_t, Node*>> queue;
     std::unordered_set<IdType> visited;
+    size_t child_idx = 0;
+    for (auto& child : tree[root.id]) {
+        root.children.emplace_back(child.first, &root, child.second);
+        queue.push_back(std::make_pair(child_idx, &root));
+        child_idx += 1;
+    }
+    visited.insert(root.id);
 
     while (!queue.empty()) {
-        Node* curr_node = queue.front();
+        Node* parent_node;
+        size_t curr_idx;
+        std::tie(curr_idx, parent_node) = queue.front();
+        Node* curr_node = &parent_node->children[curr_idx];
         queue.pop_front();
         visited.insert(curr_node->id);
 
+        child_idx = 0;
         for (auto& neighbor : tree[curr_node->id]) {
             if (visited.find(neighbor.first) == visited.end()) {
                 curr_node->children.emplace_back(neighbor.first, curr_node, neighbor.second);
-                queue.push_back(&curr_node->children.back());
+                queue.push_back(std::make_pair(child_idx, curr_node));
+                child_idx += 1;
             }
         }
+
     }
     return root;
 }
