120b9860a4e6533e80e2b74b5e68728a7f6bffa2
Lukas Stevens <mail@lukas-stevens.de>
2017-02-25 15:55:56 +0100

    Add signature struct to header

--- code/cpp/src/include/Partition.hpp
+++ code/cpp/src/include/Partition.hpp
@@ -1,19 +1,48 @@
 #pragma once
 
 #include<cstdint>
-#include<memory>
 #include<unordered_map>
 #include<string>
 #include<vector>
+
+struct Signature {
+    using CountType = uint32_t;
+    public:
+        std::vector<CountType> sig;
+
+        Signature(std::vector<CountType>);
+
+        friend inline bool operator==(const Signature& lhs, const Signature& rhs);
+        friend inline bool operator!=(const Signature& lhs, const Signature& rhs);
+
+        Signature& operator+=(const Signature& rhs);
+        friend Signature operator+(Signature lhs, const Signature& rhs);
+
+        Signature(Signature&&) = default;
+        Signature& operator=(Signature&&) = default;
+        // Delete copy constructor and copy-assignment so that no accidental copies happen
+        Signature(Signature const&) = delete;
+        Signature& operator=(Signature const&) = delete;
+};
+
+// Custom hash function for Signature
+namespace std {
+    template <> struct hash<Signature>
+    {
+        size_t operator()(const Signature & x) const;
+    };
+}
+
 
 struct Node {
     using IdType = uint32_t;
     using EdgeWeightType = uint32_t;
     public:
-        IdType id;
-        Node* parent;
+        IdType const id;
+        Node* const parent;
         std::vector<Node> children;
-        EdgeWeightType parent_edge_weight;
+        EdgeWeightType const parent_edge_weight;
+        std::unordered_map<Signature, Signature::CountType> signatures;
 
         Node(IdType, Node*, EdgeWeightType);
 
@@ -28,4 +57,5 @@ struct Node {
         Node(Node const&) = delete;
         Node& operator=(Node const&) = delete;
 };
+
 
