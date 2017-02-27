735696aa3c4775a59d00cbbdb8c76f5d21d74cd6
Lukas Stevens <mail@lukas-stevens.de>
2017-02-27 14:11:13 +0100

    Implement missing functions

--- code/cpp/src/include/Partition.hpp
+++ code/cpp/src/include/Partition.hpp
@@ -1,16 +1,25 @@
 #pragma once
 
 #include<cstdint>
+#include<map>
 #include<unordered_map>
 #include<string>
 #include<vector>
 
-struct Signature {
-    using CountType = uint32_t;
-    public:
+namespace part {
+
+    struct Signature {
+        using CountType = uint32_t;
+
+        public:
         std::vector<CountType> sig;
 
         Signature(std::vector<CountType>);
+        Signature(Signature&&) = default;
+        Signature& operator=(Signature&&) = default;
+        // Delete copy constructor and copy-assignment so that no accidental copies happen
+        Signature(Signature const&) = delete;
+        Signature& operator=(Signature const&) = delete;
 
         friend inline bool operator==(const Signature& lhs, const Signature& rhs);
         friend inline bool operator!=(const Signature& lhs, const Signature& rhs);
@@ -18,44 +27,32 @@ struct Signature {
         Signature& operator+=(const Signature& rhs);
         friend Signature operator+(Signature lhs, const Signature& rhs);
 
-        Signature(Signature&&) = default;
-        Signature& operator=(Signature&&) = default;
-        // Delete copy constructor and copy-assignment so that no accidental copies happen
-        Signature(Signature const&) = delete;
-        Signature& operator=(Signature const&) = delete;
-};
-
-// Custom hash function for Signature
-namespace std {
-    template <> struct hash<Signature>
-    {
-        size_t operator()(const Signature & x) const;
+        friend size_t hash(const Signature& signature);
     };
-}
 
+    struct Node {
+        using IdType = uint32_t;
+        using EdgeWeightType = uint32_t;
 
-struct Node {
-    using IdType = uint32_t;
-    using EdgeWeightType = uint32_t;
-    public:
+        public:
         IdType const id;
         Node* const parent;
         std::vector<Node> children;
         EdgeWeightType const parent_edge_weight;
-        std::unordered_map<Signature, Signature::CountType> signatures;
+        std::map<Signature::CountType, std::unordered_map<Signature, Signature::CountType>> signatures;
 
         Node(IdType, Node*, EdgeWeightType);
-
-        static Node build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree);
-        static Node build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree, IdType root_id);
-
-        std::string to_string();
-
         Node(Node&&) = default;
         Node& operator=(Node&&) = default;
         // Delete copy constructor and copy-assignment so that no accidental copies happen
         Node(Node const&) = delete;
         Node& operator=(Node const&) = delete;
-};
 
+        static Node build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree);
+        static Node build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree, IdType root_id);
+
+        std::string to_string();
+    };
+
+}
 
--- code/cpp/src/main/Partition.cpp
+++ code/cpp/src/main/Partition.cpp
@@ -5,64 +5,110 @@
 #include<string>
 #include<sstream>
 #include<list>
+#include<cassert>
 
 #include "Partition.hpp"
 
-Node::Node(IdType i, Node* p, EdgeWeightType w) : id(i), parent(p), parent_edge_weight(w) {}
+namespace part {
 
-Node Node::build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree, IdType root_id) {
-    Node root(root_id, nullptr, 0);
-    std::list<std::pair<size_t, Node*>> queue;
-    std::unordered_set<IdType> visited;
-    size_t child_idx = 0;
-    for (auto& child : tree[root.id]) {
-        root.children.emplace_back(child.first, &root, child.second);
-        queue.push_back(std::make_pair(child_idx, &root));
-        child_idx += 1;
+    Signature::Signature(std::vector<Signature::CountType> s) : sig(s) {}
+
+    bool operator==(const Signature& lhs, const Signature& rhs) {
+        assert(lhs.sig.size() == rhs.sig.size());
+        for (size_t i = 0; i < lhs.sig.size(); ++i) {
+            if (lhs.sig[i] != rhs.sig[i]) {
+                return false;
+            }
+        }
+        return true;
     }
-    visited.insert(root.id);
 
-    while (!queue.empty()) {
-        Node* parent_node;
-        size_t curr_idx;
-        std::tie(curr_idx, parent_node) = queue.front();
-        Node* curr_node = &parent_node->children[curr_idx];
-        queue.pop_front();
-        visited.insert(curr_node->id);
+    bool operator!=(const Signature& lhs, const Signature& rhs) {
+        return !(lhs == rhs);
+    }
 
-        child_idx = 0;
-        for (auto& neighbor : tree[curr_node->id]) {
-            if (visited.find(neighbor.first) == visited.end()) {
-                curr_node->children.emplace_back(neighbor.first, curr_node, neighbor.second);
-                queue.push_back(std::make_pair(child_idx, curr_node));
-                child_idx += 1;
+    Signature& Signature::operator+=(const Signature& rhs) {
+        assert(this->sig.size() == rhs.sig.size());
+        for (size_t i = 0; i < this->sig.size(); ++i) {
+            this->sig[i] += rhs.sig[i]; 
+        }
+        return *this;
+    }
+
+    Signature operator+(Signature lhs, const Signature& rhs) {
+        Signature lhs_copy(lhs.sig);
+        lhs_copy += rhs;
+        return lhs_copy; 
+    }
+
+    size_t hash(const Signature& signature) {
+        std::stringstream stream;
+        for (Signature::CountType const& value : signature.sig) {
+            char const* value_as_chars = static_cast<char const*>(static_cast<void const*>(&value));
+            for (size_t i = 0; i < sizeof(Signature::CountType); ++i) {
+                stream << value_as_chars[i];
             }
         }
 
+        std::hash<std::string> hash_fn;
+        return hash_fn(stream.str());
     }
-    return root;
-}
 
+    Node::Node(IdType i, Node* p, EdgeWeightType w) : id(i), parent(p), parent_edge_weight(w) {}
 
-Node Node::build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree) {
-    return build_tree(tree, tree.begin()->first);
-}
-
-std::string Node::to_string() {
-    std::stringstream stream;
-    std::list<Node*> queue;
-    queue.push_back(this);
-
-    while (!queue.empty()) {
-        Node* curr_node = queue.front();
-        queue.pop_front();
-
-        for (auto& child : curr_node->children) {
-            stream << curr_node->id << " -> " << child.id;
-            stream << "[label=\"" << child.parent_edge_weight << "\"]\n";
-            queue.push_back(&child);
+    Node Node::build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree, IdType root_id) {
+        Node root(root_id, nullptr, 0);
+        std::list<std::pair<size_t, Node*>> queue;
+        std::unordered_set<IdType> visited;
+        size_t child_idx = 0;
+        for (auto& child : tree[root.id]) {
+            root.children.emplace_back(child.first, &root, child.second);
+            queue.push_back(std::make_pair(child_idx, &root));
+            child_idx += 1;
         }
-    }
-    return stream.str();
-}
+        visited.insert(root.id);
 
+        while (!queue.empty()) {
+            Node* parent_node;
+            size_t curr_idx;
+            std::tie(curr_idx, parent_node) = queue.front();
+            Node* curr_node = &parent_node->children[curr_idx];
+            queue.pop_front();
+            visited.insert(curr_node->id);
+
+            child_idx = 0;
+            for (auto& neighbor : tree[curr_node->id]) {
+                if (visited.find(neighbor.first) == visited.end()) {
+                    curr_node->children.emplace_back(neighbor.first, curr_node, neighbor.second);
+                    queue.push_back(std::make_pair(child_idx, curr_node));
+                    child_idx += 1;
+                }
+            }
+
+        }
+        return root;
+    }
+
+
+    Node Node::build_tree(std::unordered_map<IdType, std::unordered_map<IdType, EdgeWeightType>>& tree) {
+        return build_tree(tree, tree.begin()->first);
+    }
+
+    std::string Node::to_string() {
+        std::stringstream stream;
+        std::list<Node*> queue;
+        queue.push_back(this);
+
+        while (!queue.empty()) {
+            Node* curr_node = queue.front();
+            queue.pop_front();
+
+            for (auto& child : curr_node->children) {
+                stream << curr_node->id << " -> " << child.id;
+                stream << "[label=\"" << child.parent_edge_weight << "\"]\n";
+                queue.push_back(&child);
+            }
+        }
+        return stream.str();
+    }
+}
