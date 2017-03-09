62e25b26399cb9722717249f085cdc2325739f3f
Lukas Stevens <lukas.steve@web.de>
2017-03-09 14:40:06 +0100

    Fix encoding error

--- code/cpp/.ycm_extra_conf.py
+++ code/cpp/.ycm_extra_conf.py
@@ -39,7 +39,7 @@ def LoadSystemIncludes():
     regex = re.compile(r'(?:\#include \<...\> search starts here\:)(?P<list>.*?)(?:End of search list)', re.DOTALL)
     process = subprocess.Popen(['clang', '-v', '-E', '-x', 'c++', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process_out, process_err = process.communicate('')
-    output = process_out + process_err
+    output = str(process_out + process_err, 'utf-8')
     includes = []
     for p in re.search(regex, output).group('list').split('\n'):
         p = p.strip()
