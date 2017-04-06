514eeca7e90148f49f9b9d2ebd2e9fb463d228b1
Lukas Stevens <mail@lukas-stevens.de>
2017-04-06 17:27:43 +0200

    Fix some bugs

--- code/cpp/src/main/Pack.cpp
+++ code/cpp/src/main/Pack.cpp
@@ -1,30 +1,23 @@
-#include<unordered_map>
-#include<unordered_set>
-
-#include "ValarrayUtils.hpp"
-
 #include "Pack.hpp"
-
 
 namespace pack {
 
     template<typename T>
-        std::vector<std::valarray<T>> calculate_bin_signatures(
+        std::vector<std::valarray<T>> Packing<T>::calculate_bin_signatures(
                 std::vector<T> const& component_sizes,
                 std::valarray<T> const& component_cnts,
                 T const bin_capacity) {
-            using Signature = std::valarray<T>;
-            using SignatureMap = std::unordered_map<Signature, T, valarrutils::ValarrayHasher<T>, valarrutils::ValarrayEqual<T>>;
+            using SignatureToTMap = std::unordered_map<Signature, T, valarrutils::ValarrayHasher<T>, valarrutils::ValarrayEqual<T>>;
 
-            SignatureMap old_bin_signatures;
+            SignatureToTMap old_bin_signatures;
             old_bin_signatures[Signature(component_cnts.size())] = 0;
             for (size_t idx = 0; idx < component_sizes.size(); ++idx) {
-                SignatureMap bin_signatures;
+                SignatureToTMap bin_signatures;
                 for (auto const& old_bin_signature : old_bin_signatures) {
-                    for (T comp_cnt = 0; comp_cnt < component_cnts[idx]; ++comp_cnt) {
+                    for (T comp_cnt = 0; comp_cnt <= component_cnts[idx]; ++comp_cnt) {
                         Signature bin_signature = old_bin_signature.first;
                         bin_signature[idx] += comp_cnt;
-                        T const bin_signature_component_size = old_bin_signature.second + component_sizes[idx];
+                        T const bin_signature_component_size = old_bin_signature.second + comp_cnt * component_sizes[idx];
                         if (bin_signature_component_size <= bin_capacity) {
                             bin_signatures[bin_signature] = bin_signature_component_size;
                         } 
@@ -51,45 +44,50 @@ namespace pack {
         }
 
     template<typename T>
-        std::pair<bool, std::valarray<T>> calculate_partial_packing(
-                std::unordered_map<std::valarray<T>, std::valarray<T>> const& prev_partial_packing, 
-                std::unordered_map<std::valarray<T>, std::valarray<T>>& curr_partial_packing,
+        bool Packing<T>::calculate_partial_packing(
+                SignatureToSignatureMap const& prev_partial_packing, 
+                SignatureToSignatureMap& curr_partial_packing,
                 std::vector<std::valarray<T>> const& bin_signatures) {
 
             for (auto const& prev_packing_signature : prev_partial_packing) {
                 for (auto const& bin_signature : bin_signatures) {
-                    std::valarray<T> packing_signature = prev_packing_signature.first - bin_signature; 
-                    curr_partial_packing[packing_signature] = prev_packing_signature.first;
+                    Signature packing_signature = prev_packing_signature.first - bin_signature; 
                     if (packing_signature.max() <= 0) {
-                        return std::make_pair(true, packing_signature);
+                        packing_signature = 0;
+                        curr_partial_packing[packing_signature] = prev_packing_signature.first;
+                        return true;
                     }
+                    curr_partial_packing[packing_signature] = prev_packing_signature.first;
                 }
             }
-            return std::make_pair(false, std::valarray<T>());
+            return false;
         }
 
     template<typename T>
         void Packing<T>::pack_perfect(std::map<T, T> const& components) {
 
-            std::vector<T> component_sizes;
-            std::valarray<T> component_cnts;
-            for (auto const& component : components) {
-                component_sizes.push_back(component.first);
-                component_cnts.push_back(component.second);
+            std::vector<T> component_sizes(components.size());
+            Signature component_cnts(components.size());
+            {
+                size_t idx = 0;
+                for (auto const& component : components) {
+                    component_sizes[idx] = component.first;
+                    component_cnts[idx] = component.second;
+                    idx += 1;
+                }
             }
-            
-            auto bin_signatures = calculate_bin_signatures(component_sizes, component_cnts, this->bin_capacity);
 
-            std::vector<std::unordered_map<Signature, Signature>> partial_packings(1);
+            auto bin_signatures = calculate_bin_signatures(component_sizes, component_cnts, this->opt_bin_capacity);
+
+            std::vector<std::unordered_map<Signature, Signature, valarrutils::ValarrayHasher<T>, valarrutils::ValarrayEqual<T>>> partial_packings(1);
             partial_packings[0][component_cnts] = component_cnts;
-            bool found_packing = false;
-            Signature complete_packing;
-            for(size_t idx = 1; !found_packing; ++idx) {
-                partial_packings.push_back(std::unordered_set<Signature>());
-                std::tie(found_packing, complete_packing) = 
-                    calculate_partial_packing(partial_packings[idx - 1], partial_packings[idx], bin_signatures);
+            bool is_packing_complete = false;
+            for(size_t idx = 1; !is_packing_complete; ++idx) {
+                partial_packings.emplace_back();
+                is_packing_complete = calculate_partial_packing(partial_packings[idx - 1], partial_packings[idx], bin_signatures);
             }
 
+            Signature complete_packing(component_sizes.size());
             for (size_t bin_idx = partial_packings.size() - 1; bin_idx > 0; --bin_idx) {
                 Signature curr_bin_signature = partial_packings[bin_idx][complete_packing] - complete_packing;
                 std::vector<T> curr_bin;
@@ -104,7 +102,7 @@ namespace pack {
 
     template<typename T>
         void Packing<T>::expand_packing(std::map<T, std::vector<T>> const& component_size_mapping) {
-            std::map<T, std::pair<T, T>> comp_size_mapping_cp(component_size_mapping);
+            std::map<T, std::vector<T>> comp_size_mapping_cp(component_size_mapping);
 
             for (auto const& bin : this->bins) {
                 for (auto& comp : bin) {
@@ -139,7 +137,7 @@ namespace pack {
                     idx += 1;
                 }
                 if (!comp_is_packed) {
-                    this->bins.push_back(std::move(std::vector<T>()));
+                    this->bins.emplace_back();
                     this->bins.back().push_back(comp);
                     remaining_bin_capacities.push_back(this->approx_bin_capacity - comp);
                 }
