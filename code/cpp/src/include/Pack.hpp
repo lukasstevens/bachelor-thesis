294005e251e88b3a080b2ff864a6061dcd4881e3
Lukas Stevens <mail@lukas-stevens.de>
2017-04-06 17:27:43 +0200

    Fix some bugs

--- code/cpp/src/include/Pack.hpp
+++ code/cpp/src/include/Pack.hpp
@@ -1,6 +1,9 @@
 #include<map>
 #include<valarray>
 #include<vector>
+#include<unordered_map>
+
+#include "ValarrayUtils.hpp"
 
 // T always has to be a signed number.
 namespace pack {
@@ -10,7 +13,7 @@ namespace pack {
             public:
                 T const opt_bin_capacity;
                 T const approx_bin_capacity;
-                
+
                 using Signature = std::valarray<T>;
 
                 Packing(T const opt_bin_capacity, T const approx_bin_capacity) : 
@@ -26,6 +29,19 @@ namespace pack {
 
             private:
                 std::vector<std::vector<T>> bins;
-        };
 
+                static std::vector<std::valarray<T>> calculate_bin_signatures(
+                        std::vector<T> const& component_sizes, 
+                        Signature const& component_cnts,
+                        T const bin_capacity);
+
+                using SignatureToSignatureMap = std::unordered_map<Signature, Signature, 
+                      valarrutils::ValarrayHasher<T>, valarrutils::ValarrayEqual<T>>;
+
+                static bool calculate_partial_packing(
+                        SignatureToSignatureMap const& prev_partial_packing, 
+                        SignatureToSignatureMap& curr_partial_packing,
+                        std::vector<std::valarray<T>> const& bin_signatures); 
+
+        };
 }
