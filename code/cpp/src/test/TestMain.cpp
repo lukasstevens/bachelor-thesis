83317039ba94f809ba2a198d7458f2022d62c088
Lukas Stevens <lukas.steve@web.de>
2017-04-08 16:43:38 +0200

    Output cli args for debugging

--- code/cpp/src/test/TestMain.cpp
+++ code/cpp/src/test/TestMain.cpp
@@ -1,6 +1,13 @@
+#include<iostream>
+
 #include<gtest/gtest.h>
 
 int main(int argc, char** argv) {
+    std::cerr << "gtest cli args:";
+    for (int arg_idx = 0; arg_idx < argc; ++arg_idx) {
+        std::cerr << " " << argv[arg_idx];
+    }
+    std::cerr << std::endl;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
