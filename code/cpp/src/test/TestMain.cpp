def77d8b563cb2bc01dff048fc66a17513aaff83
Lukas Stevens <lukas.steve@web.de>
2017-05-03 16:14:49 +0200

    Remove debugging output

--- code/cpp/src/test/TestMain.cpp
+++ code/cpp/src/test/TestMain.cpp
@@ -3,11 +3,6 @@
 #include<gtest/gtest.h>
 
 int main(int argc, char** argv) {
-    std::cerr << "gtest cli args:";
-    for (int arg_idx = 0; arg_idx < argc; ++arg_idx) {
-        std::cerr << " " << argv[arg_idx];
-    }
-    std::cerr << std::endl;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
