962ab4e06d45772a14d9bc192473f46aa2781664
Lukas Stevens <mail@lukas-stevens.de>
2017-05-16 13:13:35 +0200

    Use faster hash function

--- code/cpp/src/include/ValarrayUtils.hpp
+++ code/cpp/src/include/ValarrayUtils.hpp
@@ -22,39 +22,33 @@ namespace valarrutils {
              * @param s the valarray to hash.
              * @returns The hash.
              */
-            size_t operator()(const std::valarray<T>& s) const {
-                // Interpret the underlying vector of the signature as a bytestream. Convert the bytestream
-                // to a std::string and then hash it with the default hash.
-                std::string stream;
-                for (T const& value : s) {
-                    char const* value_as_chars = static_cast<char const*>(static_cast<void const*>(&value));
-                    for (size_t i = 0; i < sizeof(T); ++i) {
-                        stream.push_back(value_as_chars[i]);
-                    }
-                }
+            size_t operator()(const std::valarray<T>& arr) const {
+                // For an explanation see http://stackoverflow.com/a/27216842
+				std::size_t seed = arr.size();
+				for(auto& entry : arr) {
+					seed ^= entry + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+				}
+				return seed;
+			}
+		};
 
-                std::hash<std::string> hash_fn;
-                return hash_fn(stream);
-            }
-        };
+	/**
+	 * Compares two valarrays for equality since the operator == returns a valarray<bool>.
+	 * @tparam T The integer type to use.
+	 */
+	template<typename T>
+		struct ValarrayEqual {
 
-    /**
-     * Compares two valarrays for equality since the operator == returns a valarray<bool>.
-     * @tparam T The integer type to use.
-     */
-    template<typename T>
-        struct ValarrayEqual {
-
-            /**
-             * Tests two valarrays for equality.
-             * @param lhs The first valarray.
-             * @param rhs The second valarray.
-             * @returns True if the operands are equal.
-             */
-            bool operator()(const std::valarray<T>& lhs, const std::valarray<T>& rhs) const {
-                auto comp = lhs == rhs;
-                // min returns false if at least one member is false.
-                return comp.min();
-            }
-        };
+			/**
+			 * Tests two valarrays for equality.
+			 * @param lhs The first valarray.
+			 * @param rhs The second valarray.
+			 * @returns True if the operands are equal.
+			 */
+			bool operator()(const std::valarray<T>& lhs, const std::valarray<T>& rhs) const {
+				auto comp = lhs == rhs;
+				// min returns false if at least one member is false.
+				return comp.min();
+			}
+		};
 }
