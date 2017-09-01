556558905cdcf0ff45a86fb63fade9e2f09bb098
Lukas Stevens <mail@lukas-stevens.de>
2017-09-01 19:40:40 +0200

    Continue with analysis of runtime

--- thesis/data/trees/runtime/kparts/fat/all.bash
+++ thesis/data/trees/runtime/kparts/fat/all.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 export CODE_DIR=$(cd ../../../.. && source code_dir.bash)
 source ../../../../tables.bash
+source ../params.bash
 
-kparts=(3 4 5 6 8 10)
 flags='-g tree_fat -m Tree_Partition -o time --min_child=2 --max_child=11'
-kparts_table kparts 50 3 10 20 "${flags}" &
-kparts_table kparts 50 1 4 20 "${flags}" &
-kparts_table kparts 50 1 5 20 "${flags}" &
+kparts_table kparts ${first[n]} ${first[ie]} ${first[id]} ${tries} "${flags}" &
+kparts_table kparts ${second[n]} ${second[ie]} ${second[id]} ${tries} "${flags}" &
+kparts_table kparts ${third[n]} ${third[ie]} ${third[id]} ${tries} "${flags}" &
 wait
