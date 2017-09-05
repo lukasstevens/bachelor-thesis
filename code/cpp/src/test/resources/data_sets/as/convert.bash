8d826049c96b9dd32ecf0cf0b1fc13766ead072e
Lukas Stevens <mail@lukas-stevens.de>
2017-09-05 23:12:01 +0200

    Add all datasets

---
+++ code/cpp/src/test/resources/data_sets/as/convert.bash
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cat as19990829.txt | ../../../../../build/bachelorthesis-1.0.0/snapconvert > as19990829.graph
---
+++ code/cpp/src/test/resources/data_sets/ca-GrQc/convert.bash
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cat ca-GrQc.txt | ../../../../../build/bachelorthesis-1.0.0/snapconvert > ca-GrQc.graph
